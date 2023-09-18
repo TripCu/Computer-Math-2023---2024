22ffdce7edc480520412983e9fc009836b7ebe19
Trip cubbison <119246941+TripCu@users.noreply.github.com>
2023-09-18 10:38:59 -0400

    Create racer.java

---
+++ shells/Units/Unit1/racer.java
@@ -0,0 +1,34 @@
+public class Racer extends Athlete
+ {
+  public Racer(int y)
+ {
+ super(1, y, Display.EAST,
+Display.INFINITY);
+ }
+ public void jumpRight()
+ {
+//pseudocode: tL, m, tR, m, tR, m, tL
+ }
+ public void jumpLeft()
+ {
+ }
+ public void sprint(int n)
+ {
+ for(int k=1; k <= n; k++)
+ move();
+ }
+ public void put(int n)
+ {
+
+ }
+ public void pick(int n)
+ {
+
+ }
+ public void shuttle(int spaces,
+int beepers)
+
+41 {
+42
+43
+44 }
