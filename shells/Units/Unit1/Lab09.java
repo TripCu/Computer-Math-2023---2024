c34c6540040d804ce9f5eed789ee5efa80414fe9
Trip cubbison <119246941+TripCu@users.noreply.github.com>
2023-10-28 00:25:58 -0400

    Update Lab09.java

--- shells/Units/Unit1/Lab09.java
+++ shells/Units/Unit1/Lab09.java
@@ -3,54 +3,59 @@ import edu.fcps.karel2.Robot;
 import javax.swing.JOptionPane;
 
 public class Lab09 {
-   public static void main(String[] args) {
-      String filename = JOptionPane.showInputDialog("What robot world?");
+    public static void main(String[] args) {
+        String filename = JOptionPane.showInputDialog("What robot world?");
          
-      Display.openWorld("maps/" + filename + ".map");
-      Display.setSize(10, 10);
-      Display.setSpeed(5);
+        // Initialize the world and robot
+        Display.openWorld("maps/" + filename + ".map");
+        Display.setSize(10, 10);
+        Display.setSpeed(5);
    
-      Athlete robo = new Athlete(1, 1, Display.EAST, 0); 
+        Athlete robo = new Athlete(1, 1, Display.EAST, 0); 
 
-      int pile1 = 0;
-      int pile2 = 0;
+        // Initialize two piles for beepers
+        int pile1 = 0;
+        int pile2 = 0;
       
-      // Move the robot until it finds a beeper
-      while (!robo.nextToABeeper()) {
-         robo.move();
-      }
+        // Move the robot until it finds a beeper
+        while (!robo.nextToABeeper()) {
+            robo.move();
+        }
       
-      int a;
-      for (a = 1; a <= 4; a++) {
-         // Collect beepers into pile1 and put beepers from pile2
-         while (robo.nextToABeeper()) {
-            robo.pickBeeper();
-            pile1++;
-         }
-         while (pile2 > 0) {
-            robo.putBeeper();
-            pile2--;
-         }
+        // Perform a series of actions four times
+        for (int a = 1; a <= 4; a++) {
+            // Collect beepers into pile1 and put beepers from pile2
+            while (robo.nextToABeeper()) {
+                robo.pickBeeper();
+                pile1++;
+            }
+            while (pile2 > 0) {
+                robo.putBeeper();
+                pile2--;
+            }
       
-         robo.move();
+            // Move the robot forward
+            robo.move();
            
-         // Collect beepers into pile2 and put beepers from pile1
-         while (robo.nextToABeeper()) {
-            robo.pickBeeper();
-            pile2++;
-         }
-         while (pile1 > 0) {
-            robo.putBeeper();
-            pile1--;
-         }
-         robo.move();
-      }
+            // Collect beepers into pile2 and put beepers from pile1
+            while (robo.nextToABeeper()) {
+                robo.pickBeeper();
+                pile2++;
+            }
+            while (pile1 > 0) {
+                robo.putBeeper();
+                pile1--;
+            }
+            
+            // Move the robot forward for the next iteration
+            robo.move();
+        }
       
-      // Turn the robot around and move it to the starting position
-      robo.turnAround();
-      while (robo.frontIsClear()) {
-         robo.move();
-      }
-      robo.turnAround();
+        // Turn the robot around and move it to the starting position
+        robo.turnAround();
+        while (robo.frontIsClear()) {
+            robo.move();
+        }
+        robo.turnAround();
    }
 }
