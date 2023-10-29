ca2b28b60f6f6f7ab1036a1d1645831898e3a70a
Trip cubbison <119246941+TripCu@users.noreply.github.com>
2023-10-28 23:39:45 -0400

    ok

--- shells/Units/Unit1/racer.java
+++ shells/Units/Unit1/racer.java
@@ -1,51 +1,59 @@
-import edu.fcps.karel2.Display;
-import edu.fcps.karel2.Robot; 
 
+import edu.fcps.karel2.Robot;
+import edu.fcps.karel2.Display;
 public class Racer extends Athlete
 {
    public Racer(int y)
    {
-      super(1, y, Display.EAST,
-         Display.INFINITY);
-   }
-   public void jumpRight()
-   {
-      turnLeft();
-      move();
-      turnRight();
-      move();
-      turnRight();
-      move();
+      super(1, y, Display.EAST, Display.INFINITY);
    }
    public void jumpLeft()
    {
       turnLeft();
       move();
-      turnLeft();
-      move();
-      turnLeft();
+      turnRight();
       move();
       turnRight();
+      move();
+      turnLeft();
    }
-
+   public void jumpRight()
+   {
+      turnRight();
+      move();
+      turnLeft();
+      move();
+      turnLeft();
+      move();
+      turnRight();      
+      
+   }
    public void sprint(int n)
    {
-      for(int k=1; k <= n; k++)
+      for(int k=1; k<= n; k++)
          move();
-   }
-   public void put(int n)
-   {
-   
    }
    public void pick(int n)
    {
-   
+      for(int k=1; k<= n; k++)
+       pickBeeper();
    }
-   public void shuttle(int spaces,
-   int beepers)
-   
+   public void put(int n)
    {
-   
+      for(int k=1; k<= n; k++)
+       putBeeper();
    }
-
+   public void shuttle(int spaces, int beepers)
+   {
+   move();
+   jumpLeft();
+   sprint(spaces);
+   pick(beepers);
+   turnAround();
+   sprint(spaces);
+   jumpRight();
+   move();
+   put(beepers);
+   turnAround();
+   }
 }
