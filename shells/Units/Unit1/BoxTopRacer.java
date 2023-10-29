12ee4e2639592d5d856ceb6e00a9c233de198466
Trip cubbison <119246941+TripCu@users.noreply.github.com>
2023-10-28 23:04:43 -0400

    Add files via upload

---
+++ shells/Units/Unit1/BoxTopRacer.java
@@ -0,0 +1,72 @@
+//Thomas Gibbs 10/12/23
+import edu.fcps.karel2.Robot;
+import edu.fcps.karel2.Display;
+public class BoxTopRacer extends Racer
+{
+   public BoxTopRacer(int y)
+   {
+      super(1);
+   }
+   public void jumpLeft()
+   {
+      int up = 0;
+      turnLeft();
+      while(!rightIsClear())
+      {
+         move();
+         up++;
+      }
+      turnRight();
+      move();
+      while(!rightIsClear())
+      {
+         move();
+      }
+      turnRight();
+      for(int i = 1; i <= up; i++)
+      {
+         move();
+      }
+      turnLeft();
+      
+   }
+   public void jumpRight()
+   {
+      turnRight();
+      move();
+      turnLeft();
+      move();
+      turnLeft();
+      move();
+      turnRight();      
+      
+   }
+   public void sprint(int n)
+   {
+      for(int k=1; k<= n; k++)
+         move();
+   }
+   public void pick(int n)
+   {
+      for(int k=1; k<= n; k++)
+       pickBeeper();
+   }
+   public void put(int n)
+   {
+      for(int k=1; k<= n; k++)
+       putBeeper();
+   }
+   public void shuttle(int spaces, int beepers)
+   {
+   move();
+   jumpLeft();
+   sprint(spaces);
+   pick(beepers);
+   turnAround();
+   sprint(spaces);
+   jumpRight();
+   move();
+   put(beepers);
+   turnAround();
+   }
+}
