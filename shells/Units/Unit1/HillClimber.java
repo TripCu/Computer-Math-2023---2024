7a396f99b580a2d01b46088feebd92203f8e94d9
Trip cubbison <119246941+TripCu@users.noreply.github.com>
2023-10-29 00:49:48 -0400

    Add files via upload

--- shells/Units/Unit1/HillClimber.java
+++ shells/Units/Unit1/HillClimber.java
@@ -1,46 +1,51 @@
-//Thomas 8/31/23
 import edu.fcps.karel2.Display;
 import edu.fcps.karel2.Robot;
-public class HillClimber extends Climber
+
+public class HillClimber extends Climber 
 {
    public HillClimber()
-   {
-      super();
-   }
-   public HillClimber(int x)
-   {
-      super(x);
-   }
-   public void climbUpRight()
-   {
-      turnLeft();
-      move();
-      turnRight();
-      move();
-      move();
-   }
-   public void climbDownRight()
-   {
-      move();
-      move();
-      turnRight();
-      move();
-      turnLeft();
-   }
+  {
+   super(8, 1, Display.NORTH, 1);
+  }
+   public HillClimber(int x, int y, int dir, int beep)
+  {
+   super(x, y, dir, beep);
+  }
+  
    public void climbUpLeft()
-   {
-      turnRight();
-      move();
-      turnLeft();
-      move();
-      move();
-   }
+  {
+   turnRight();
+   move();
+   turnLeft();
+   move();
+   move();
+  }
+  
    public void climbDownLeft()
-   {
-      move();
-      move();
-      turnLeft();
-      move();
-      turnRight();
-   }
+  {
+   move();
+   move();
+   turnLeft();
+   move();
+   turnRight();
+  }
+  
+   public void climbUpRight()
+  {
+   turnLeft();
+   move();
+   turnRight();
+   move();
+   move();
+  }
+  
+   public void climbDownRight()
+  {
+   move();
+   move();
+   turnRight();
+   move();
+   turnLeft();
+  }
+  
 }
