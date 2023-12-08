7dc649998050116d5c3278ea9ad83aa512c24634
Trip cubbison <119246941+TripCu@users.noreply.github.com>
2023-12-08 12:17:50 -0500

    ordering code

--- shells/Units/Unit2/Graphics Project 2023/GraphicImage.java
+++ shells/Units/Unit2/Graphics Project 2023/GraphicImage.java
@@ -3,7 +3,6 @@ import java.awt.*;
 
 
 public class GraphicImage extends JPanel {
-
     public static final int CLEAR_SCREEN = -1;
     public static final int VERTICAL_LINES = 0;
     public static final int HORIZONTAL_LINES = 1;
@@ -23,27 +22,18 @@ public class GraphicImage extends JPanel {
     public static final int BRICKS = 15;
     public static final int PIZZA = 16;
     public static final int PENCIL = 17;
-
-
     private int pictureName = CLEAR_SCREEN;
-
-
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         drawPicture(g, pictureName);
         repaint();
     }
-
-
     public void setPictureName(int name) {
         pictureName = name;
     }
-
     public void drawPicture(Graphics g, int pictureName) {
-
         clearScreen(g);
-
         switch (pictureName) {
             case VERTICAL_LINES -> drawVerticalLines(g);
             case HORIZONTAL_LINES -> drawHorizontalLines(g);
@@ -66,50 +56,31 @@ public class GraphicImage extends JPanel {
             default -> clearScreen(g);
         }
     }
-
-
     /**
      * Clears the screen
-     *
-     * @param g the graphic
      */
     public void clearScreen(Graphics g) {
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, 500, 500);
     }
-
-
     /**
      * Draws 7 - 15 vertical lines equidistant apart
-     *
-     * @param g the graphic
      */
     public void drawVerticalLines(Graphics g) {
         g.setColor(Color.CYAN);
         for (int x = 0; x <= 500; x += 50)
             g.drawRect(x, 0, 50, 500);
-
-
     }
-
-
     /**
      * Draws 7 - 15 horizontal lines equidistant apart
-     *
-     * @param g the graphic
      */
     public void drawHorizontalLines(Graphics g) {
         g.setColor(Color.yellow);
         for (int x = 0; x <= 500; x += 50)
             g.drawLine(500, x, 0, x);
-
     }
-
-
     /**
      * Draw 15 - 30 diagonal lines equidistant apart
-     *
-     * @param g the graphic
      */
     public void drawDiagonalLines(Graphics g) {
         g.setColor(Color.red);
@@ -119,14 +90,9 @@ public class GraphicImage extends JPanel {
             g.drawLine(500, x, x, 500);
         }
             g.drawLine(0,500,500,0);
-
     }
-
-
     /**
      * Draw at least 15 lines all converging to the top-left corner
-     *
-     * @param g the graphic
      */
     public void drawCornerLines(Graphics g) {
         g.setColor(new Color(100, 100, 255));
@@ -136,87 +102,56 @@ public class GraphicImage extends JPanel {
         for(int cline = 500; cline >= 0; cline-=50){
             g.drawLine(0, 0, 500, cline);
         }
-
     }
-
     /**
      * Draw the webbing pattern that appears in the top-right corner.
-     *
-     * @param g the graphic
      */
     public void drawWebTopRight(Graphics g) {
         g.setColor(Color.pink);
         int x;
         for (x = 0; x <= 500; x += 50) {
             g.drawLine(x, 0, 500, x);
-
         }
-
     }
-
-
     /**
      * Draw the webbing pattern that appears in the top-left corner.
-     *
-     * @param g the graphic
      */
     public void drawWebTopLeft(Graphics g) {
         g.setColor(Color.pink);
         int x;
         for (x = 0; x <= 500; x += 50) {
             g.drawLine(0, x, (500-x), 0);
-
         }
-
     }
-
-
     /**
      * Draw the webbing pattern that appears in the center of the screen.
-     *
-     * @param g the graphic
-     *
-     *
-     *
-     *
-     *
      */
     public void drawWebCenter(Graphics g) {
         for(int y =250; y >= 0; y-= 25)
         {
             g.setColor(Color.pink);
             g.drawLine(250, y, (250 + y), 250);
-
         }
 
         for(int y =250; y <= 500; y+= 25)
         {
             g.setColor(Color.pink);
             g.drawLine(250, y, (750 - y), 250);
-
         }
 
         for(int y =250; y <= 500; y+= 25)
         {
             g.setColor(Color.pink);
             g.drawLine(250, y, (y- 250), 250);
-
-
-
         }
         for(int y =250; y >= 0; y-= 25)
         {
             g.setColor(Color.pink);
             g.drawLine(250, y, (250 - y), 250);
-
         }
     }
-
-
     /**
      * Draw a starburst of lines that appear to come from the center of the screen.
-     *
-     * @param g the graphic
      */
     public void drawStarburst(Graphics g) {
         for( int l = 0; l<= 500; l += 36)
@@ -225,33 +160,19 @@ public class GraphicImage extends JPanel {
             g.drawLine(0, l,500,500-l);
             g.drawLine(l, 0,500-l,500);
         }
-
-
     }
-
-
     /**
      * Draw at least 5 vertical bars of the same width across the screen.
-     *
-     * @param g the graphic
      */
     public void drawVerticalBars(Graphics g) {
-
         g.setColor(Color.red);
-
             for (int i = 0; i <= 500; i+= 100) {
                 g.fillRect( i,0,50, 500);
-
             }
-
-
     }
-
 
     /**
      * Draw the triangle pattern (similar to Tri-force from Zelda games).
-     *
-     * @param g the graphic
      */
     public void drawTriangles(Graphics g) {
         g.setColor(Color.YELLOW);
@@ -264,16 +185,9 @@ public class GraphicImage extends JPanel {
         int[] x3Points = {250, 370, 500};
         int[] y3Points = {500, 250, 500};
         g.fillPolygon(x3Points, y3Points, 3);
-
-
     }
-
-
-
     /**
      * Draw a bullseye with at least 5 circles.
-     *
-     * @param g the graphic
      */
     public void drawBullseye(Graphics g) {
         int c = 0;
@@ -289,63 +203,44 @@ public class GraphicImage extends JPanel {
             g.fillOval(x/2, x/2, 500-x, 500-x);
         }
     }
-
-
     /**
      * Create a 8x8 square checkerboard
-     *
-     * @param g the Graphics object
      */
     public void drawCheckerboard(Graphics g) {
-
         int rows = 8;
         int cols = 8;
         int cellSize = getWidth() / cols;
-
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 int x = col * cellSize;
                 int y = row * cellSize;
-
                 if ((row + col) % 2 == 0) {
                     g.setColor(Color.RED);
                 } else {
                     g.setColor(Color.BLACK);
                 }
-
                 g.fillRect(x, y, cellSize, cellSize);
             }
         }
     }
-
-
     /**
-     * Draw at least a grid of 4 x 4 dots, centered properly on the screen.
-     *
      * @param g the graphic
      */
     public void drawDots(Graphics g) {
         int rows = 5;
         int cols = 5;
         int dotSize = 40;
-
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
                 int x = col * getWidth() / (cols - 1);
                 int y = row * getHeight() / (rows - 1);
-
                 g.setColor(Color.pink);
                 g.fillOval(x+30, y+30, dotSize, dotSize);
             }
         }
     }
-
-
-
     /**
      * Draw a proportional 5 point star.
-     *
-     * @param g the graphic
      */
     public void drawPacMan(Graphics g) {
         int centerX = getWidth() / 2;
@@ -365,12 +260,8 @@ public class GraphicImage extends JPanel {
         g.setColor(Color.BLACK);
         g.fillOval(centerX - eyeOffsetX, centerY - eyeOffsetY, eyeSize, eyeSize);
     }
-
-
     /**
      * Draw an American Flag to scale.  You may use circles for the stars.
-     *
-     * @param g the graphic
      */
     public void drawAmericanFlag(Graphics g) {
         int stripeHeight = getHeight() / 13;
@@ -380,9 +271,9 @@ public class GraphicImage extends JPanel {
             } else {
                 g.setColor(Color.WHITE);
             }
-            g.fillRect(0, i * stripeHeight, getWidth(), stripeHeight);
-            g.setColor(Color.BLUE);
-            g.fillRect(0, 0, getWidth() /2 , stripeHeight * 6);
+                g.fillRect(0, i * stripeHeight, getWidth(), stripeHeight);
+                g.setColor(Color.BLUE);
+            g   .fillRect(0, 0, getWidth() /2 , stripeHeight * 6);
             }
         g.setColor(Color.WHITE);
         for (int r = 0; r < 7; r++) {
@@ -392,16 +283,9 @@ public class GraphicImage extends JPanel {
                 g.fillOval(x+10, y+10, 20, 20);
             }
         }
-        }
-
-
-
-
-
+    }
     /**
      * Draw an alternating pattern that resembles bricks on a wall.
-     *
-     * @param g the graphic
      */
     public void drawBricks(Graphics g) {
         for (int i = 0; i < 25; i++) {
@@ -412,7 +296,6 @@ public class GraphicImage extends JPanel {
                 if (i % 2 != 0) {
                     x -= 50 / 2;
                 }
-
                 g.setColor(Color.RED);
                 g.fillRect(x, y, 50, 20);
                 g.setColor(Color.WHITE);
@@ -420,22 +303,16 @@ public class GraphicImage extends JPanel {
             }
         }
     }
-
-
-
-
     /**
      * Draw a slice of pizza - include crust, cheese,
      * and pepperoni (other toppings optional)
-     *
-     * @param g the graphic
      */
     public void drawPizza(Graphics g) {
         //Use fill arc instead of this
         g.setColor(new Color(244, 203, 66));
         g.fillOval(50,0,500,500);
         g.setColor(Color.YELLOW);
-        g.fillOval(75,15,500,500);
+        g.fillOval(75,15,450,450); //500 //500
         g.setColor(Color.BLACK);
         g.fillRect(0,250,500,250);
         int[] xPoints = {300, 0, 500, 500};
@@ -446,19 +323,11 @@ public class GraphicImage extends JPanel {
         g.fillOval(145,170,30,30);
         g.fillOval(180,205,25,25);
         g.fillOval(125,210,25,25);
-
         //g.fillArc(50,5,100,100,75,-40);
-
-
-
     }
-
-
     /**
      * Draw a pencil, include an eraser, the metal thing that attaches
      * the eraser, pencil, and point.
-     *
-     * @param g the graphic
      */
     public void drawPencil(Graphics g) {
         int centerX = getWidth() / 2;
@@ -466,11 +335,10 @@ public class GraphicImage extends JPanel {
 
         // Draw pencil body
         g.setColor(Color.YELLOW);
-        g.fillRect(centerX - 100, centerY - 20, 100, 40);
-
+        g.fillRect(centerX - 100, centerY - 20, 300, 100);
         // Draw pencil eraser
         g.setColor(Color.PINK);
-        g.fillArc(centerX - 120, centerY - 20, 40, 40, 90, 180);
+        g.fillArc(centerX - 115, centerY - 20, 40, 40, 90, 180);
 
         Color LightBrown = new Color(244, 203, 66);
         g.setColor(LightBrown);
