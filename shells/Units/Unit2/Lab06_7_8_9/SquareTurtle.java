0c4e1f22a6a48e809b962bb4aba432ff9bf53580
Trip cubbison <119246941+TripCu@users.noreply.github.com>
2023-12-13 22:57:07 -0500

    hep

--- shells/Units/Unit2/Lab06_7_8_9/SquareTurtle.java
+++ shells/Units/Unit2/Lab06_7_8_9/SquareTurtle.java
@@ -1,44 +1,40 @@
-	//Name______________________________ Date_____________
-   import edu.fcps.Turtle;
-    public class SquareTurtle extends Turtle
-   {
-      private double mySize;
-   
-   	//*************************constructors*************************
-       public SquareTurtle()
-      {
-         super();
-         mySize = 50.0;
-      }
-       public SquareTurtle(double n)
-      {
-         super();
-         mySize = n;
-      }
-       public SquareTurtle(double x, double y, double h)
-      {
-         super(x, y, h);
-         mySize = 50.0;
-      }
-       public SquareTurtle(double x, double y, double h, double n)
-      {
-         super(x, y, h);
-         mySize = n;
-      }
-   
-   	//************************methods*****************************
-       public void setSize(double n)
-      {
-         mySize = n;
-      }
-       public void drawShape()
-      {
-      
-         /************************/
-      	/*                      */
-      	/* Your code goes here. */
-      	/*                      */
-      	/************************/
-      
-      }
-   }
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import edu.fcps.Turtle;
+
+public class SquareTurtle extends Turtle {
+    private double mySize;
+
+    public SquareTurtle() {
+        this.mySize = 50.0;
+    }
+
+    public SquareTurtle(double n) {
+        this.mySize = n;
+    }
+
+    public SquareTurtle(double x, double y, double h) {
+        super(x, y, h);
+        this.mySize = 50.0;
+    }
+
+    public SquareTurtle(double x, double y, double h, double n) {
+        super(x, y, h);
+        this.mySize = n;
+    }
+
+    public void setSize(double n) {
+        this.mySize = n;
+    }
+
+    public void drawShape() {
+        for(int k = 1; k <= 4; ++k) {
+            this.forward(this.mySize);
+            this.turnLeft(90.0);
+        }
+
+    }
+}
