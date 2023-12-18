ef6f8343b73a6e0705dde04ea0767954c9a38714
Trip cubbison <119246941+TripCu@users.noreply.github.com>
2023-12-18 10:30:46 -0500

    hhh

--- shells/Units/Unit2/Lab06_7_8_9/Lab07/PolygonTurtle.java
+++ shells/Units/Unit2/Lab06_7_8_9/Lab07/PolygonTurtle.java
@@ -1,39 +1,63 @@
 package Lab06_7_8_9.Lab07;    //Name______________________________ Date_____________
+
 import edu.fcps.Turtle;
-public class PolygonTurtle extends Turtle
-{
+
+public class PolygonTurtle extends Turtle {
+
     private double mySize;
+
     private int mySides;
-    public PolygonTurtle()
-    {
+
+    public PolygonTurtle() {
+
         super();
+
         mySize = 50.0;
+
         mySides = 6;
+
     }
-    public PolygonTurtle(double n, int s)
-    {
+
+    public PolygonTurtle(double n, int s) {
+
         mySize = n;
+
         mySides = s;
+
     }
-    public PolygonTurtle(double x, double y, double h, double n, int s)
-    {
+
+    public PolygonTurtle(double x, double y, double h, double n, int s) {
+
         super(x, y, h);
+
         mySize = n;
+
         mySides = s;
+
     }
-    public void setSize(double n)
-    {
+
+    public void setSize(double n) {
+
         mySize = n;
+
     }
-    public void setSides(int s)
-    {
+
+    public void setSides(int s) {
+
         mySides = s;
+
     }
-    public void drawShape()
-    {
-        for (int i =1; i <= 4; i++){
+
+    public void drawShape() {
+
+        for (int i = 1; i <= 4; i++) {
+
             forward(mySize);
-            turnLeft(360/mySides);
+
+            turnLeft(360 / mySides);
+
         }
+
     }
+
 }
