c4a35f94e7a79b240f8d12278c1ef91ebf9df84f
Trip cubbison <119246941+TripCu@users.noreply.github.com>
2023-12-20 09:16:11 -0500

    Update music.bat

--- shells/Units/Unit2/Christmas_Tree_Compettion/music.bat
+++ shells/Units/Unit2/Christmas_Tree_Compettion/music.bat
@@ -2,4 +2,5 @@
 start  "" "C:\Users\wolfp\OneDrive\Documents\GitHub\Computer-Math-2023---2024\shells\Units\Unit2\Christmas_Tree_Compettion\Mariah Carey - All I Want For Christmas Is You.mp3"
 :: String batchFilePath = "C:\\Users\\wolfp\\OneDrive\\Documents\\GitHub\\Computer-Math-2023---2024\\shells\\Units\\Unit2\\Christmas_Tree_Compettion\\music.bat";
 :: command to run batch file Process process = Runtime.getRuntime().exec(batchFilePath);
-::public static void main(String[] args) throws IOException {
+::public static void main(String[] args) throws IOException {  import java.io.IOException;
+
