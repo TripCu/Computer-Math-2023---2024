10f1b8983feecae4e55870aeed3ced998a31b77b
Trip cubbison <119246941+TripCu@users.noreply.github.com>
2024-01-18 10:00:28 -0500

    works

---
+++ shells/Units/Euler Problems/problem.java
@@ -0,0 +1,11 @@
+class problem {
+    public static void main(String[] args) {
+        int result = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                result += i;
+            }
+        }
+        System.out.println(result);
+    }
+}
