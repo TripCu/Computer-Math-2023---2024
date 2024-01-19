65285a1fd6c9a9b20c35e59dd7080806123816b7
Trip cubbison <119246941+TripCu@users.noreply.github.com>
2024-01-19 16:34:49 -0500

    lol

---
+++ shells/Units/Unit2/Lab04/Driver4b.java
@@ -0,0 +1,28 @@
+package Lab04;
+
+import edu.fcps.Bucket;
+import javax.swing.JFrame;
+
+public class Driver4b {
+    public Driver4b() {
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Buckets");
+        frame.setSize(600, 400);
+        frame.setLocation(100, 100);
+        frame.setDefaultCloseOperation(3);
+        frame.setContentPane(new BucketPanel());
+        frame.setVisible(true);
+        Bucket.setSpeed(1);
+        Bucket.useTotal(false);
+        Bucket five = new Bucket(5);
+        Bucket three = new Bucket(3);
+        five.fill();
+        five.pourInto(three);
+        three.spill();
+        five.pourInto(three);
+        five.fill();
+        five.pourInto(three);
+    }
+}
--- shells/Units/Unit2/Lab16/PrizePanel.java
+++ shells/Units/Unit2/Lab16/PrizePanel.java
@@ -1,74 +1,101 @@
 package Lab16;
-
-import Lab12.Ball;
-import Lab12.Polkadot;
-
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class PrizePanel extends JPanel
-{
-  private static final int FRAME = 400;
-  private static final Color BACKGROUND = new Color(204, 204, 204);
-  private BufferedImage myImage;
-  private Graphics myBuffer;
-  private Lab12.Ball ball;
-  private Lab12.Polkadot pd;
-  private Timer t;
-  private int hits;
-    //constructor
-   public PrizePanel()
-  {
-      myImage =  new BufferedImage(FRAME, FRAME, BufferedImage.TYPE_INT_RGB);
-      myBuffer = myImage.getGraphics();
-      myBuffer.setColor(BACKGROUND);
-      myBuffer.fillRect(0, 0, FRAME,FRAME);
-      int xPos = (int)(Math.random()*(FRAME-100) + 50);
-      int yPos = (int)(Math.random()*(FRAME-100)+ 50);
-      ball = new Lab12.Ball(xPos, yPos, 50, Color.BLACK);
-      pd = new Lab12.Polkadot();
-      hits= 0 ;
-      t = new Timer(5, new Listener());
-      t.start();
-  }
-   public void paintComponent(Graphics g)
-   {
-       g.drawImage(myImage, 0, 0, getWidth(), getHeight(), null);
-   }
+public class PrizePanel extends JPanel {
+    private static final int FRAME_WIDTH = 400;
+    private static final Color BACKGROUND = new Color(204, 204, 204);
+    private static final Color BALL_COLOR;
+    private static final Color PRIZE_COLOR;
+    private static final int BALL_DIAM = 50;
+    private static final int PRIZE_DIAM = 25;
+    private static final int PRIZE_SHIFT = 10;
+    private BufferedImage myImage = new BufferedImage(400, 400, 1);
+    private Graphics myBuffer;
+    private Ball ball;
+    private Polkadot prize;
+    private Timer t;
+    private int hits;
 
+    public PrizePanel() {
+        this.myBuffer = this.myImage.getGraphics();
+        this.myBuffer.setColor(BACKGROUND);
+        this.myBuffer.fillRect(0, 0, 400, 400);
+        this.ball = new Ball(0, 0, 50, BALL_COLOR);
+        this.ball.jump(400, 400);
+        this.prize = new Polkadot(0, 0, 25, PRIZE_COLOR);
+        this.prize.jump(400, 400);
+        this.hits = 0;
+        this.t = new Timer(5, new Listener());
+        this.t.start();
+        this.addMouseListener(new Mouse());
+    }
 
-   private class Listener implements ActionListener
-  {
-      public void actionPerformed(ActionEvent e)
-     {
-             myBuffer.setColor(BACKGROUND);
-             myBuffer.fillRect(0,0,FRAME,FRAME);
-             ball.move(FRAME, FRAME);
-             collide(ball, pd);
-             ball.draw(myBuffer);
-             pd.draw(myBuffer);
-             myBuffer.setColor(Color.BLACK);
-             myBuffer.setFont(new Font("Monospaced", Font.BOLD, 24));
-             myBuffer.drawString("Count: " + hits, FRAME - 150, 25);
-             repaint();
-     }
-  }
+    public void paintComponent(Graphics g) {
+        g.drawImage(this.myImage, 0, 0, this.getWidth(), this.getHeight(), (ImageObserver)null);
+    }
 
+    public void collide(Ball b, Polkadot p) {
+        double dist = this.distance((double)b.getX(), (double)b.getY(), (double)p.getX(), (double)p.getY());
+        if (dist < 37.0) {
+            ++this.hits;
+            p.jump(400, 400);
+        }
 
-   private void collide(Ball b, Polkadot pd)
-  {
-    double d = distance(b.getX(), b.getY(), pd.getX(), pd.getY());
-    if( d < b.getRadius() + pd.getRadius())
-      {
-        hits++;
-        pd.jump(FRAME,FRAME);
-      }
-  }
-   private double distance(double x1, double y1, double x2, double y2)
-  {
-     return Math.sqrt( Math.pow(x2-x1,2) + Math.pow(y2-y1,2)); 	 // enter the calculation here.
-  }
+    }
+
+    private double distance(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x1 - x2, 2.0) + Math.pow(y1 - y2, 2.0));
+    }
+
+    static {
+        BALL_COLOR = Color.BLACK;
+        PRIZE_COLOR = Color.RED;
+    }
+
+    private class Listener implements ActionListener {
+        private Listener() {
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            PrizePanel.this.myBuffer.setColor(PrizePanel.BACKGROUND);
+            PrizePanel.this.myBuffer.fillRect(0, 0, 400, 400);
+            PrizePanel.this.ball.move(400, 400);
+            PrizePanel.this.collide(PrizePanel.this.ball, PrizePanel.this.prize);
+            PrizePanel.this.ball.draw(PrizePanel.this.myBuffer);
+            PrizePanel.this.prize.draw(PrizePanel.this.myBuffer);
+            PrizePanel.this.myBuffer.setColor(Color.BLACK);
+            PrizePanel.this.myBuffer.setFont(new Font("Monospaced", 1, 24));
+            PrizePanel.this.myBuffer.drawString("Count: " + PrizePanel.this.hits, 250, 25);
+            PrizePanel.this.repaint();
+        }
+    }
+
+    private class Mouse extends MouseAdapter {
+        private Mouse() {
+        }
+
+        public void mouseClicked(MouseEvent e) {
+            if (e.isMetaDown()) {
+                PrizePanel.this.ball.setX(e.getX());
+                PrizePanel.this.ball.setY(e.getY());
+            } else if (e.isShiftDown()) {
+                PrizePanel.this.ball.setdx((int)(Math.random() * 11.0 - 6.0));
+                PrizePanel.this.ball.setdy((int)(Math.random() * 11.0 - 6.0));
+            } else {
+                PrizePanel.this.prize.setX(e.getX());
+                PrizePanel.this.prize.setY(e.getY());
+            }
+
+        }
+    }
 }
