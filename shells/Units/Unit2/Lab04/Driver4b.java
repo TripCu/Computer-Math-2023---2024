b94f964cadf4ed4d7b2f5b39737824ea75bc923f
Trip cubbison <119246941+TripCu@users.noreply.github.com>
2024-01-19 20:49:11 -0500

    qwe

--- shells/Units/Unit2/Lab04/Driver4b.java
+++ shells/Units/Unit2/Lab04/Driver4b.java
@@ -4,25 +4,36 @@ import edu.fcps.Bucket;
 import javax.swing.JFrame;
 
 public class Driver4b {
-    public Driver4b() {
-    }
-
     public static void main(String[] args) {
         JFrame frame = new JFrame("Buckets");
         frame.setSize(600, 400);
         frame.setLocation(100, 100);
-        frame.setDefaultCloseOperation(3);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setContentPane(new BucketPanel());
         frame.setVisible(true);
         Bucket.setSpeed(1);
         Bucket.useTotal(false);
+        Bucket three  = new Bucket(3);
+        Bucket four = new Bucket(4);
         Bucket five = new Bucket(5);
-        Bucket three = new Bucket(3);
         five.fill();
         five.pourInto(three);
         three.spill();
         five.pourInto(three);
         five.fill();
         five.pourInto(three);
+        five.pourInto(four);
+        three.spill();
+        five.pourInto(three);
+        five.fill();
+        five.pourInto(three);
+        four.pourInto(three);
+        five.fill();
+        five.pourInto(four);
+        four.pourInto(three);
+        three.spill();
+        five.pourInto(four);
+        four.pourInto(three);
+        three.spill();
     }
 }
