3d4c20130c0a73632be3897fc5dbc07a2cce00be
Trip cubbison <119246941+TripCu@users.noreply.github.com>
2024-02-05 22:45:16 -0500

    lol

--- shells/Units/Unit2/Breakout2024/Breakout.java
+++ shells/Units/Unit2/Breakout2024/Breakout.java
@@ -1,273 +1,223 @@
 package Breakout2024;
-import acm.graphics.*;
-import acm.program.*;
-import acm.util.*;
-import java.applet.*;
+
+import acm.graphics.GLabel;
+import acm.graphics.GObject;
+import acm.graphics.GOval;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.MouseEvent;
 
-public class Breakout extends GraphicsProgram
-{
+public class Breakout extends GraphicsProgram {
+    private GRect paddle;
+    private GOval ball;
+    private double vx;
+    private double vy;
+    private int scoreCount = 0;
+    private int bricksLeft = 100;
+    private int gameLives = 3;
+    private GLabel score;
+    private GLabel amountLeft;
+    private GLabel lives;
+    
 
-	//width of game display
-	private static final int WIDTH = 400;
+    public static void main(String[] args) {
+        String[] sizeArgs = new String[]{"width=400", "height=600"};
+        (new Breakout()).start(sizeArgs);
+    }
 
-	//height of game display
-	private static final int HEIGHT = 600;
+    public void run() {
+        this.score = new GLabel("Score: " + this.scoreCount);
+        this.score.isVisible();
+        this.score.setLocation(50.0, 50.0);
+        this.add(this.score);
+        this.lives = new GLabel("Lives: " + this.gameLives);
+        this.lives.isVisible();
+        this.lives.setLocation(150.0, 50.0);
+        this.add(this.lives);
+        this.amountLeft = new GLabel("Bricks Left: " + this.bricksLeft);
+        this.amountLeft.isVisible();
+        this.amountLeft.setLocation(300.0, 50.0);
+        this.add(this.amountLeft);
+        this.setup();
+        this.play();
+    }
 
-	//width of paddle
-	private static final int PADDLE_WIDTH = 60;
+    public void setup() {
+        this.createBricks();
+        this.createPaddle();
+        this.createBall();
+        this.addMouseListeners();
+    }
 
-	//height of paddle
-	private static final int PADDLE_HEIGHT = 10;
+    public void createBricks() {
+        for (int x = 0; x < 10; ++x) {
+            for (int y = 0; y < 10; ++y) {
+                GRect brick = new GRect(y * 36 + 4 * y + 2, 70 + 8 * x + 4 * x, 36.0, 8.0);
+                brick.setFilled(true);
+                if (x == 0 || x == 1) {
+                    brick.setColor(Color.RED);
+                }
 
-	//offset of paddle up from the bottom
-	private static final int PADDLE_Y_OFFSET = 30;
+                if (x == 2 || x == 3) {
+                    brick.setColor(Color.ORANGE);
+                }
 
-	//number of bricks per row
-	private static final int NBRICKS_PER_ROW = 10;
+                if (x == 4 || x == 5) {
+                    brick.setColor(Color.YELLOW);
+                }
 
-	//number of rows of bricks
-	private static final int NBRICK_ROWS = 10;
+                if (x == 6 || x == 7) {
+                    brick.setColor(Color.GREEN);
+                }
 
-	//separation between bricks
-	private static final int BRICK_SEP = 4;
+                if (x == 8 || x == 9) {
+                    brick.setColor(Color.CYAN);
+                }
 
-	//width of each brick (based on the dimensions of the game display)
-	private static final int BRICK_WIDTH = WIDTH / NBRICKS_PER_ROW - BRICK_SEP;
+                this.add(brick);
+            }
+        }
 
-	//height of brick
-	private static final int BRICK_HEIGHT = 8;
+    }
 
-	//radius of ball in pixels
-	private static final int BALL_RADIUS = 6;
+    public void createPaddle() {
+        this.paddle = new GRect(170.0, 560.0, 60.0, 10.0);
+        this.paddle.setFilled(true);
+        this.add(this.paddle);
+    }
 
-	//offset of the top brick row from top
-	private static final int BRICK_Y_OFFSET = 70;
+    public void createBall() {
+        this.ball = new GOval(194.0, 294.0, 12.0, 12.0);
+        this.ball.setFilled(true);
+        this.add(this.ball);
+    }
 
-	//number of turns
-	private static final int NTURNS = 3;
+    public void play() {
+        this.waitForClick();
+        this.startTheBall();
+        this.playBall();
+    }
 
-	//the paddle
-	private GRect paddle;
+    public void startTheBall() {
+        RandomGenerator rgen = new RandomGenerator();
+        this.vy = 0.3;
+        this.vx = rgen.nextDouble(0.1, 0.4);
+        if (rgen.nextBoolean(0.5)) {
+            this.vx = -this.vx;
+        }
 
-	//the ball
-	private GOval ball;
+    }
 
-	//ball velocity in both directions (x-direction, and y-direction)
-	private double vx, vy;
+    public void playBall() {
+        while (true) {
+            this.ball.move(this.vx, this.vy);
+            this.pause(1.0);
+            if (this.ball.getY() <= -6.0) {
+                this.vy *= -1.0;
+            } else if (this.ball.getX() >= 394.0) {
+                this.vx *= -1.0;
+            } else if (this.ball.getX() <= -6.0) {
+                this.vx *= -1.0;
+            } else if (this.ball.getY() >= 594.0) {
+                this.vy *= -1.0;
+                --this.gameLives;
+                this.lives.setLabel("Lives: " + this.gameLives);
+                if (this.gameLives >= 1) {
+                    this.remove(this.ball);
+                    this.createBall();
+                    this.waitForClick();
+                    this.startTheBall();
+                } else {
+                    this.remove(this.ball);
+                    this.remove(this.paddle);
+                    GLabel loss = new GLabel("Game Over", 200.0, 300.0);
+                    loss.isVisible();
+                    loss.setColor(Color.BLACK);
+                    this.add(loss);
+                    this.pause(5000.0);
+                    this.gameLives = 3;
+                    this.scoreCount = 0;
+                    this.bricksLeft = 100;
+                    this.remove(loss);
+                    this.setup();
+                    this.waitForClick();
+                    this.play();
+                }
+            }
 
-	//records the last x position of the mouse (see mouseMoved method)
-	private double lastX;
+            GObject collider = this.getCollidingObject();
+            if (collider == this.paddle) {
+                if (this.vx > 0.0) {
+                    if (this.ball.getX() < collider.getX() + 30.0) {
+                        this.vx = -this.vx;
+                    }
+                } else if (this.ball.getX() > collider.getX() + 30.0) {
+                    this.vx = -this.vx;
+                }
 
-	//used for mouse events (only moves the paddle every 5th mouse move)
-	private int toggle = 0;
+                this.vy = -this.vy;
+            } else if (collider instanceof GRect) {
+                this.vy = -this.vy;
+                this.remove(collider);
+                this.score.setLabel("Score: " + this.scoreCount);
+                this.amountLeft.setLabel("Bricks Left: " + this.bricksLeft);
+                --this.bricksLeft;
+                this.amountLeft.setLabel("Bricks Left: " + this.bricksLeft);
 
-	//main method -- called when the program is run
-	public static void main(String[] args)
-	{
-		String[] sizeArgs = { "width=" + WIDTH, "height=" + HEIGHT };
-		new Breakout().start(sizeArgs);
-	}
+                if (collider.getColor() == Color.CYAN) {
+                    this.scoreCount += 2;
+                    this.score.setLabel("Score: " + this.scoreCount);
+                } else if (collider.getColor() == Color.GREEN) {
+                    this.scoreCount += 5;
+                    this.score.setLabel("Score: " + this.scoreCount);
+                } else if (collider.getColor() == Color.YELLOW) {
+                    this.scoreCount += 7;
+                    this.score.setLabel("Score: " + this.scoreCount);
+                } else if (collider.getColor() == Color.ORANGE) {
+                    this.scoreCount += 10;
+                    this.score.setLabel("Score: " + this.scoreCount);
+                } else if (collider.getColor() == Color.RED) {
+                    this.scoreCount += 10;
+                    this.score.setLabel("Score: " + this.scoreCount);
+                }
 
-	//run method -- called indirectly from the main method
-	public void run()
-	{
-		setup();
-		play();
-	}
+                if (this.bricksLeft < 1) {
+                    GLabel winner = new GLabel("WINNER!", 200.0, 300.0);
+                    winner.isVisible();
+                    winner.setColor(Color.GREEN);
+                    this.add(winner);
+                    this.remove(this.ball);
+                    this.pause(5000.0);
+                    this.remove(winner);
+                    this.setup();
+                    this.waitForClick();
+                    this.play();
+                }
 
-	//setup method -- called from the run method
-	public void setup()
-	{
-		createBricks();
-		createPaddle();
-		createBall();
-		addMouseListeners();
-	}
+                this.remove(collider);
+            }
+        }
+    }
 
-	//createBricks method -- called from the setup method
-	public void createBricks()
-	{
-		//make the bricks
-		for(int x = 0; x < NBRICK_ROWS; x++)
-		{
-			for(int y = 0; y < NBRICKS_PER_ROW; y++)
-			{
-				GRect brick = new GRect((y * BRICK_WIDTH) + BRICK_SEP*y + BRICK_SEP/2,
-											BRICK_Y_OFFSET + (BRICK_HEIGHT * x) + BRICK_SEP*x,
-												BRICK_WIDTH,
-													BRICK_HEIGHT);
-				brick.setFilled(true);
+    private GObject getCollidingObject() {
+        if (this.getElementAt(this.ball.getX(), this.ball.getY()) != null) {
+            return this.getElementAt(this.ball.getX(), this.ball.getY());
+        } else if (this.getElementAt(this.ball.getX() + 12.0, this.ball.getY()) != null) {
+            return this.getElementAt(this.ball.getX() + 12.0, this.ball.getY());
+        } else if (this.getElementAt(this.ball.getX() + 12.0, this.ball.getY() + 12.0) != null) {
+            return this.getElementAt(this.ball.getX() + 12.0, this.ball.getY() + 12.0);
+        } else {
+            return this.getElementAt(this.ball.getX(), this.ball.getY() + 12.0) != null ? this.getElementAt(this.ball.getX(), this.ball.getY() + 12.0) : null;
+        }
+    }
 
-                /*                       				  */
-         		/* add code to correctly color the bricks */
-         		/*                      				  */
-         		/******************************************/
+    public void mouseMoved(MouseEvent e) {
+        if (e.getX() < this.getWidth() - 30 && e.getX() > 30) {
+            this.paddle.setLocation(e.getX() - 30, this.getHeight() - 30 - 10);
+        }
 
-
-				add(brick);
-			}
-		}
-	}
-
-	//createPaddle method -- called from the setup method
-	public void createPaddle()
-	{
-		/******************************************/
-        /*                       				  */
-        /* add code to create and add the paddle  */
-        /*                      				  */
-        /******************************************/
-	}
-
-	//createBall method -- called from the setup method
-	public void createBall()
-	{
-		/******************************************/
-        /*                       				  */
-        /* add code to create and add the ball    */
-        /*                      				  */
-        /******************************************/
-	}
-
-	//play method -- called from the run method after setup
-	public void play()
-	{
-		startTheBall();
-		playBall();
-	}
-
-	//startTheBall method -- called from the play method
-	public void startTheBall()
-	{
-
-		/******************************************/
-        /*                      				  */
-        /* create a random double between 1 and 3,*/
-        /* (between 1.0 and 2.99) for vx          */
-        /*                      				  */
-        /* flip a coin for left or right direction*/
-        /* (make vx positive or negative)   	  */
-        /*                      				  */
-        /* set initial y velocity to 3.0          */
-        /*                      				  */
-        /******************************************/
-
-	}
-
-	//playBall method -- called from the play method
-	public void playBall()
-	{
-		//continuous loop
-		while(true)
-		{
-			//move the ball
-			ball.move(vx, vy);
-			//pause
-			pause(1);
-
-			//check for contact along the outer walls
-
-			/**********************************************/
-        	/*                       			          */
-        	/* if ball contacts the floor,		          */
-        	/* reverse the y velocity 				      */
-        	/*                      				      */
-        	/* otherwise, if ball contacts the ceiling,	  */
-        	/* reverse the y velocity 				      */
-        	/*                      				      */
-        	/* otherwise, if ball contacts the left wall, */
-        	/* reverse the x velocity 				      */
-        	/*                      				      */
-        	/* otherwise, if ball contacts the right wall,*/
-        	/* reverse the x velocity 				      */
-        	/*                      				      */
-        	/**********************************************/
-
-
-			//check for collisions with bricks or paddle
-			GObject collider = getCollidingObject();
-
-			//if the ball collided with the paddle
-			if(collider == paddle)
-			{
-				//reverse the y velocity
-				vy = -vy;
-			}
-			//otherwise if the ball collided with a brick
-			else if(collider instanceof GRect)
-			{
-				//reverse y velocity
-				vy = -vy;
-				//remove the brick
-				remove(collider);
-			}
-		}
-	}
-
-	//getCollidingObject -- called from the playBall method
-	//discovers and returns the object that the ball collided with
-	private GObject getCollidingObject()
-	{
-		if(getElementAt(ball.getX(), ball.getY()) != null)
-			return getElementAt(ball.getX(), ball.getY());
-		else if(getElementAt(ball.getX()+BALL_RADIUS*2, ball.getY()) != null)
-			return getElementAt(ball.getX()+BALL_RADIUS*2, ball.getY());
-		else if(getElementAt(ball.getX()+BALL_RADIUS*2, ball.getY()+BALL_RADIUS*2) != null)
-			return getElementAt(ball.getX()+BALL_RADIUS*2, ball.getY()+BALL_RADIUS*2);
-		else if(getElementAt(ball.getX(), ball.getY()+BALL_RADIUS*2) != null)
-			return getElementAt(ball.getX(), ball.getY()+BALL_RADIUS*2);
-		else
-			return null;
-	}
-
-	//mouseMoved method -- called by the mouseListener when the mouse is moved
-	//anywhere within the boundaries of the run window
-	public void mouseMoved(MouseEvent e)
-	{
-		//only move the paddle every 5th mouse event
-		//otherwise the play slows every time the mouse moves
-		if(toggle == 5)
-		{
-			//get the x-coordinate of the mouse
-			double eX = e.getX();
-
-			//if the mouse moved to the right
-	        if(eX - lastX > 0)
-	        {
-	        	//if paddle is not already at the right wall
-	        	if(paddle.getX() < WIDTH - PADDLE_WIDTH)
-	        	{
-	        		//move to the right
-	        		paddle.move(eX - lastX, 0);
-	        	}
-	        }
-	        else //(if the mouse moved to the left)
-	        {
-	        	//if paddle is not already at the left wall
-	        	if(paddle.getX() > 0)
-	        	{
-	        		//move to the left
-	        		paddle.move(eX - lastX, 0);
-	        	}
-	        }
-
-	        //record this mouse x position for next mouse event
-	        GPoint last = new GPoint(e.getPoint());
-			lastX = last.getX();
-
-			//reset toggle to 1
-			toggle = 1;
-		}
-		else
-		{
-			//increment toggle by 1
-			//(when toggle gets to 5 the code will move the paddle
-			// and reset toggle back to 1)
-			toggle++;
-		}
-
-	}
-
+    }
 }
