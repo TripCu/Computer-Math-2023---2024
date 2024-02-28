48b4a99d9fa023223f58bec16f823f8ec4384711
Trip cubbison <119246941+TripCu@users.noreply.github.com>
2024-02-28 10:49:31 -0500

    lk;ubuoyb9

--- shells/Units/Unit3/Lab00/Panel00.java
+++ shells/Units/Unit3/Lab00/Panel00.java
@@ -2,6 +2,12 @@ package Lab00;
 
 import javax.swing.*;
 import net.miginfocom.swing.*;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 /*
  * Created by JFormDesigner on Wed Feb 28 10:22:30 EST 2024
  */
@@ -11,7 +17,9 @@ import net.miginfocom.swing.*;
 /**
  * @author FrameUser
  */
-public class Panel00 extends JPanel {
+public class Panel00<Panel00$1> extends JPanel {
+    private JLabel label;
+
     public Panel00() {
         initComponents();
     }
@@ -19,26 +27,59 @@ public class Panel00 extends JPanel {
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
         // Generated using JFormDesigner Evaluation license - Trip Cubbison
+        Goooood = new JTextField();
+        checkBox1 = new JCheckBox();
 
         //======== this ========
-        setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing. border .EmptyBorder (
-        0, 0 ,0 , 0) ,  "JF\u006frm\u0044es\u0069gn\u0065r \u0045va\u006cua\u0074io\u006e" , javax. swing .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder
-        . BOTTOM, new java. awt .Font ( "D\u0069al\u006fg", java .awt . Font. BOLD ,12 ) ,java . awt. Color .
-        red ) , getBorder () ) );  addPropertyChangeListener( new java. beans .PropertyChangeListener ( ){ @Override public void propertyChange (java .
-        beans. PropertyChangeEvent e) { if( "\u0062or\u0064er" .equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } );
+        setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(
+        new javax.swing.border.EmptyBorder(0,0,0,0), "JF\u006frmDes\u0069gner \u0045valua\u0074ion"
+        ,javax.swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM
+        ,new java.awt.Font("D\u0069alog",java.awt.Font.BOLD,12)
+        ,java.awt.Color.red), getBorder())); addPropertyChangeListener(
+        new java.beans.PropertyChangeListener(){@Override public void propertyChange(java.beans.PropertyChangeEvent e
+        ){if("\u0062order".equals(e.getPropertyName()))throw new RuntimeException()
+        ;}});
         setLayout(new MigLayout(
             "hidemode 3",
             // columns
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
+            "[fill]" +
             "[fill]" +
             "[fill]",
             // rows
             "[]" +
             "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
             "[]"));
+
+        //---- Goooood ----
+        Goooood.setText("yes");
+        add(Goooood, "cell 0 3");
+
+        //---- checkBox1 ----
+        checkBox1.setText("text");
+        add(checkBox1, "cell 0 6");
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+        //Trips code 
+        this.setLayout(new FlowLayout());
+        this.label = new JLabel("0.0000000000000000");
+        this.label.setFont(new Font("Serif", 1, 20));
+        this.label.setForeground(Color.blue);
+        this.add(this.label);
+        JButton button = new JButton("Random");
+        this.add(button);
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
     // Generated using JFormDesigner Evaluation license - Trip Cubbison
+    private JTextField Goooood;
+    private JCheckBox checkBox1;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
