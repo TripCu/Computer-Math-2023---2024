09dea2ecd72a258373ce3691e3a39d417c3d3d89
Trip cubbison <trip.cubbison@gmail.com>
2024-04-01 20:09:04 -0400

    asd

--- shells/Units/Unit3/Lab04/Odometer.java
+++ shells/Units/Unit3/Lab04/Odometer.java
@@ -1,34 +1,42 @@
-	//Name______________________________ Date_____________
-   import javax.swing.*;
-   import java.awt.*;
-    public class Odometer extends JPanel
-   {
-     /****************************/
-     /*                          */
-     /* Declare 3 fields.        */
-     /*
-     /* Declare 1 int variable.  */
-     /*                          */
-     /****************************/
-   
-       public Odometer()
-      {
-         	/*******************************/
-         	/* This is the constructor.    */
-         	/* Set the layout.				 */
-				/* Set the background.         */
-         	/* Instantiate all objects and */
-      		/* set their properties.       */
-      		/*	                            */ 
-         	/*******************************/
-      }
-       public void update()
-      {
-         	/***************************/
-         	/*                         */
-         	/* Increment the counter,  */
-         	/* set the labels.         */
-      		/*                         */          
-         	/***************************/
-      }
-   }
+package Lab04;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Odometer extends JPanel {
+    private JLabel label1;
+    private JLabel label2;
+    private JLabel label3;
+    private int total;
+
+    public Odometer() {
+        this.setLayout(new FlowLayout());
+        this.setBackground(Color.black);
+        Font f = new Font("Serif", 1, 30);
+        this.total = 0;
+        this.label1 = new JLabel("0");
+        this.label1.setFont(f);
+        this.label1.setForeground(Color.white);
+        this.add(this.label1);
+        this.label2 = new JLabel("0");
+        this.label2.setFont(f);
+        this.label2.setForeground(Color.white);
+        this.add(this.label2);
+        this.label3 = new JLabel("0");
+        this.label3.setFont(f);
+        this.label3.setForeground(Color.black);
+        this.label3.setBackground(Color.white);
+        this.label3.setOpaque(true);
+        this.add(this.label3);
+    }
+
+    public void update() {
+        ++this.total;
+        this.label3.setText("" + this.total % 10);
+        this.label2.setText("" + this.total % 100 / 10);
+        this.label1.setText("" + this.total % 1000 / 100);
+    }
+}
